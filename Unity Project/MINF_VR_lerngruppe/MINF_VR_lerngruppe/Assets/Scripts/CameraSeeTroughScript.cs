a0b742db0d93e30add60ed8b5013b131b15b6fda
Alain Dechtjar <alain.dechtjar@haw-hamburg.de>
2017-11-23 15:51:46 +0100

    [ADDED] AR / VR Button Toggle

--- Unity Project/MINF_VR_lerngruppe/MINF_VR_lerngruppe/Assets/Scripts/CameraSeeTroughScript.cs
+++ Unity Project/MINF_VR_lerngruppe/MINF_VR_lerngruppe/Assets/Scripts/CameraSeeTroughScript.cs
@@ -8,41 +8,20 @@ public class CameraSeeTroughScript : MonoBehaviour {
     private bool camAvailable;
     private WebCamTexture backCam;
     private Texture defaultBackground;
+    private bool cameraOn;
 
-    //public Text text;
+    public Text text;
     public RawImage background;
     public AspectRatioFitter fit;
+    public Canvas canvas;
 
 	// Use this for initialization
 	void Start () {
-        defaultBackground = background.texture;
-        WebCamDevice[] devices = WebCamTexture.devices;
+        InitCamera();
 
-        if(devices.Length == 0)
-        {
-            //text.text = "No Camera detected";
-            camAvailable = false;
-            return;
-        }
-
-        for (int i = 0; i < devices.Length; i++)
-        {
-            if(!devices[i].isFrontFacing)
-            {
-                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
-            }
-        }
-
-        if(backCam == null)
-        {
-            //text.text =  "Unable to find back camera";
-            return;
-        }
-
-        backCam.Play();
-        background.texture = backCam;
-
-        camAvailable = true;
+        cameraOn = false;
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        text.text = canvas.renderMode.ToString();
 	}
 	
 	// Update is called once per frame
@@ -63,5 +42,50 @@ public class CameraSeeTroughScript : MonoBehaviour {
 
         int orient = -backCam.videoRotationAngle;
         background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+
+        if (!cameraOn && OVRInput.Get(OVRInput.Button.Any))
+        {
+            text.text = canvas.renderMode.ToString();
+            cameraOn = true;
+            canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        }
+        else if (cameraOn && OVRInput.Get(OVRInput.Button.Any))
+        {
+            text.text = canvas.renderMode.ToString();
+            cameraOn = false;
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        }
 	}
+
+    void InitCamera()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            //text.text = "No Camera detected";
+            camAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (!devices[i].isFrontFacing)
+            {
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        if (backCam == null)
+        {
+            //text.text =  "Unable to find back camera";
+            return;
+        }
+
+        backCam.Play();
+        background.texture = backCam;
+
+        camAvailable = true;
+    }
 }
