893569558809afae4def445387a931abb8a930fb
Alain Dechtjar <alain.dechtjar@haw-hamburg.de>
2017-12-19 17:13:54 +0100

    Imported Scripts from VR Demo

---
+++ Unity Project/MINF_VR_lerngruppe/MINF_VR_lerngruppe/Assets/Scripts/PhotonAvatarView.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[RequireComponent (typeof(PhotonView))]
+public class PhotonAvatarView : MonoBehaviour {
+	private PhotonView _pv;
+	private OvrAvatar _ovrAvatar;
+	private OvrAvatarRemoteDriver _ovrAvatarRemoteDriver;
+	
+	private List<byte[]> _packetData = new List<byte[]> (); // outgoing buffer
+
+	private int _localSequence = 0;                         // local sequence number
+
+	private bool _isMine = false;                           // was this object controlled by me in the last Update cycle?
+
+	void Awake () {
+		_pv = GetComponent<PhotonView> ();
+		_ovrAvatar = GetComponent<OvrAvatar> ();
+		_ovrAvatarRemoteDriver = GetComponent<OvrAvatarRemoteDriver> ();
+	}
+
+	void OnEnable () {
+		_isMine = _pv.isMine;
+		if (_pv.isMine) {
+			SetPacketRecording (true);
+		}
+	}
+	
+	void OnDisable () {
+		_isMine = _pv.isMine;
+		if (_pv.isMine) {
+			SetPacketRecording (false);
+		}
+	}
+	
+	void Update () {
+		// PhotonView might switch owner at runtime, this is used to catch that
+		if (_isMine != _pv.isMine) {
+			SetPacketRecording (_pv.isMine);
+			_isMine = _pv.isMine;
+		}
+	}
+
+	/**
+	 * Toggles the recording of the local avatar
+	 */
+	void SetPacketRecording (bool recording) {
+		_ovrAvatar.RecordPackets = recording;
+		if (recording) {
+			_ovrAvatar.PacketRecorded += OnLocalAvatarPacketRecorded;
+		} else {
+			_ovrAvatar.PacketRecorded -= OnLocalAvatarPacketRecorded;
+		}
+	}
+
+	/**
+	 * Callback to process recorded avatar movements
+	 */
+	public void OnLocalAvatarPacketRecorded (object sender, OvrAvatar.PacketEventArgs args) {
+		using (MemoryStream outputStream = new MemoryStream ()) {
+			BinaryWriter writer = new BinaryWriter (outputStream);
+
+			// Bring the recorded data in a sendable binary format
+			var size = Oculus.Avatar.CAPI.ovrAvatarPacket_GetSize (args.Packet.ovrNativePacket);
+			byte[] data = new byte[size];
+			Oculus.Avatar.CAPI.ovrAvatarPacket_Write (args.Packet.ovrNativePacket, size, data);
+
+			// Use BinaryWriter to generate byte sequence
+			writer.Write (_localSequence++);           // local sequence number
+			writer.Write (size);                       // size of the following data in bytes
+			writer.Write (data);                       // the recorded avatar data
+
+			_packetData.Add (outputStream.ToArray ()); // add the package to the queue
+		}
+	}
+	
+	/**
+	 * Read the recorded avatar data from a remote avatar and queue it for playback
+	 */
+	private void DeserializeAndQueuePacketData (byte[] data) {
+		using (MemoryStream inputStream = new MemoryStream (data)) {
+			BinaryReader reader = new BinaryReader (inputStream);
+			int remoteSequence = reader.ReadInt32 (); // retrieve the remote sequence number
+
+			int size = reader.ReadInt32 ();           // retrieve the size for the rest of the data
+			byte[] sdkData = reader.ReadBytes (size); // retrieve the binary avatar data
+			
+			// Use the AvatarSDK to import the remote avatar data
+			System.IntPtr packet = Oculus.Avatar.CAPI.ovrAvatarPacket_Read ((System.UInt32) data.Length, sdkData);
+			
+			// Queue data for playback
+			_ovrAvatarRemoteDriver.QueuePacket (remoteSequence, new OvrAvatarPacket {ovrNativePacket = packet});
+		}
+	}
+	
+	/**
+	 * Called when Photon feels the need to send or recieve some data
+	 * (there is some heuristic at work here and it's not guaranteed to be called each frame)
+	 */
+	public void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
+		if (stream.isWriting) {
+			if (_packetData.Count == 0) {
+				return;
+			}
+
+			stream.SendNext (_packetData.Count);
+
+			foreach (byte[] b in _packetData) {
+				stream.SendNext (b);
+			}
+
+			_packetData.Clear ();
+		} else {
+			int num = (int) stream.ReceiveNext ();
+			
+			for (int counter = 0; counter < num; ++counter) {
+				try {
+					byte[] data = (byte[]) stream.ReceiveNext ();
+					DeserializeAndQueuePacketData (data);
+				} catch (InvalidCastException e) {
+					Debug.LogError ("InvalidCastException in PhotonAvatarView::OnPhotonSerializeView()");
+				}
+			}
+		}
+	}
+}
--- Unity Project/MINF_VR_lerngruppe/MINF_VR_lerngruppe/Assets/Scripts/PhotonController.cs
+++ Unity Project/MINF_VR_lerngruppe/MINF_VR_lerngruppe/Assets/Scripts/PhotonController.cs
@@ -1,119 +1,118 @@
-using System;
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Photon;
+using UnityEngine;
 
+/**
+ * Custom Photon Event Codes
+ * Custom Events start from 101 since [1..100] is reserved by Photon
+ */
 public enum NetworkEventCodes : byte {
 	ParticipantJoined = 101
 }
 
 public class PhotonController : PunBehaviour {
-	public String GameVersion;
+	public string GameVersion;
 	public Transform RemoteAvatarSlot;
 
 	public byte MaxPlayers = 2;
 
-	public Transform TrackingSpace;
+    public Transform TrackingSpace;
 	public GameObject LocalAvatarPrefab;
 	public GameObject RemoteAvatarPrefab;
 
-	// Use this for initialization
 	void Start () {
-		PhotonNetwork.ConnectUsingSettings(GameVersion);
+		// Players are grouped by game version, if two clients have another version than the other,
+		// the won't be able to see each other!
+		PhotonNetwork.ConnectUsingSettings (GameVersion);
 	}
 
-	void OnEnable() {
+	void OnEnable () {
 		PhotonNetwork.OnEventCall += OnEvent;
 	}
 
-	void OnDisable() {
+	void OnDisable () {
 		PhotonNetwork.OnEventCall -= OnEvent;
 	}
 
-	private void OnEvent(byte eventcode, object content, int senderid) {
-		String eventName = "UNKNOWN";
+	private void OnEvent (byte eventcode, object content, int senderid) {
+		string eventName = "UNKNOWN";
 
-		if(Enum.IsDefined (typeof (NetworkEventCodes), eventcode)) {
-			eventName = Enum.GetName(typeof (NetworkEventCodes), eventcode);
+		if (Enum.IsDefined (typeof (NetworkEventCodes), eventcode)) {
+			eventName = Enum.GetName (typeof (NetworkEventCodes), eventcode);
 		}
-
-		Debug.Log(
-			"[PhotonController]: Received event: [" +
-			"eventName = " + eventName +
-			", eventCode = " + eventcode +
-			", senderId = " + senderid +
+		
+		Debug.Log (
+			"[PhotonController]: Recieved event: [" +
+			"eventName=" + eventName + 
+			", eventCode=" + eventcode + 
+			", senderId=" + senderid + 
 			"]"
 		);
 
-		if (eventcode == (byte)NetworkEventCodes.ParticipantJoined) {
+		if (eventcode == (byte) NetworkEventCodes.ParticipantJoined) {
 			GameObject go = null;
+            
+			Debug.Log ("[PhotonController]: Executing VR avatar instantiation");
 
-			Debug.Log("[PhotonController]: Executing VR avatar instantiation");
-
-			if(PhotonNetwork.player.ID == senderid) {
-				Debug.Log("[PhotonController]: Setup local avatar for sending");
-				go = Instantiate(LocalAvatarPrefab, TrackingSpace);
-			}
-			else {
-				Debug.Log("[PhotonController]: Instantiated remote avatar");
-				if(RemoteAvatarPrefab) {
-					go = Instantiate(RemoteAvatarPrefab, TrackingSpace);
+			if (PhotonNetwork.player.ID == senderid) {
+				Debug.Log ("[PhotonController]: Setup local avatar for sending");
+				go = Instantiate (LocalAvatarPrefab, TrackingSpace);
+			} else {
+				Debug.Log ("[PhotonController]: Instantiated remote avatar");
+				if (RemoteAvatarPrefab) {
+					go = Instantiate (RemoteAvatarPrefab, RemoteAvatarSlot);
 				}
 			}
 
-			if(go != null) {
-				PhotonView pView = go.GetComponent<PhotonView>();
+			if (go != null) {
+				PhotonView pView = go.GetComponent<PhotonView> ();
 
 				if (pView != null) {
-					pView.viewID = (int)content;
+					pView.viewID = (int) content;
 				}
 			}
 		}
 	}
-
-	public override void OnConnectedToMaster() {
-		Debug.Log("[PhotonController]: Connected to Master");
-		PhotonNetwork.JoinRandomRoom();
+	
+	public override void OnConnectedToMaster () {
+		Debug.Log ("[PhotonController]: Connected to master");
+		PhotonNetwork.JoinRandomRoom ();
 	}
 
-	public override void OnJoinedLobby() {
-		Debug.Log("[PhotonController]: Lobby joined");
-		PhotonNetwork.JoinRandomRoom();
+	public override void OnJoinedLobby () {
+		Debug.Log ("[PhotonController]: Lobby joined");
+		PhotonNetwork.JoinRandomRoom ();
 	}
-
-	public void OnPhotonRandomJoinFailed() {
-		Debug.Log("[PhotonController]: Random join failed");
+	
+	public void OnPhotonRandomJoinFailed () {
+		Debug.Log ("[PhotonController]: Random join failed");
 		PhotonNetwork.CreateRoom (
 			null,
-			new RoomOptions() {
+			new RoomOptions () {
 				MaxPlayers = MaxPlayers
 			},
 			null
 		);
 	}
-
-	public override void OnFailedToConnectToPhoton(DisconnectCause cause) {
-		Debug.LogError("[PhotonController]: Failed to connect to Photon: " + cause);
+	
+	public override void OnFailedToConnectToPhoton (DisconnectCause cause) {
+		Debug.LogError ("[PhotonController]: Failed to connect to Photon: " + cause);
 	}
+	
+	public override void OnJoinedRoom () {
+		Debug.Log ("[PhotonController]: Joined room");
+		int viewId = PhotonNetwork.AllocateViewID ();
 
-	public override void OnJoinedRoom() {
-		Debug.Log("[PhotonController]: Joined room");
-		int viewId = PhotonNetwork.AllocateViewID();
-
-		PhotonNetwork.RaiseEvent(
+		PhotonNetwork.RaiseEvent (
 			(byte) NetworkEventCodes.ParticipantJoined,
 			viewId,
 			true,
-			new RaiseEventOptions() {
+			new RaiseEventOptions () {
 				CachingOption = EventCaching.AddToRoomCache,
 				Receivers = ReceiverGroup.All
 			}
 		);
-	}
-
-	// Update is called once per frame
-	void Update () {
-
 	}
 }
