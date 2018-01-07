3358cd07d5baaa8815fa87debac443680177324f
Alain Dechtjar <alain.dechtjar@haw-hamburg.de>
2018-01-07 14:48:10 +0100

    can't believe it but it works

--- Unity Project/MINF_VR_lerngruppe/MINF_VR_lerngruppe/Assets/Scripts/CameraSeeTroughScript.cs
+++ Unity Project/MINF_VR_lerngruppe/MINF_VR_lerngruppe/Assets/Scripts/CameraSeeTroughScript.cs
@@ -13,21 +13,36 @@ public class CameraSeeTroughScript : PunBehaviour {
 
     private ExitGames.Client.Photon.Hashtable hashtable = new ExitGames.Client.Photon.Hashtable();
 
-    private RawImage remotePlane;
+    private Texture2D receivedTexture;
 
-    public RawImage background;
-    public AspectRatioFitter fit;
+    private int _CaptureCounter = 0;
+
+    public RawImage localBackground;
+    public AspectRatioFitter localFitter;
+
+    public RawImage remoteBackground;
+    public AspectRatioFitter remoteFitter;
+
+    public Text localDebugText;
 
     public Canvas localCanvas;
     public Canvas remoteCanvas;
+
+    private bool player1remote;
+    private bool player2remote;
+
+    private int arrayLength;
 
 	// Use this for initialization
 	void Start () {
         InitCamera();
 
         cameraOn = true;
+        player1remote = false;
+        player2remote = false;
         localCanvas.renderMode = RenderMode.ScreenSpaceCamera;
         remoteCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        TakeSnapshot();
         //text.text = canvas.renderMode.ToString();
 	}
 	
@@ -35,16 +50,17 @@ public class CameraSeeTroughScript : PunBehaviour {
 	void Update () {
 
         if(GameObject.Find("RemoteAvatar(Clone)")){
-            remotePlane = GameObject.Find("remoteBg").GetComponent<RawImage>();
-
             Debug.Log("playerID = " + PhotonNetwork.player.ID);
 
-            if(PhotonNetwork.player.ID == 1){
-                SetTexturePlayer1();
-                ShowTexturePlayer1();
-            }else if(PhotonNetwork.player.ID == 2){
-                SetTexturePlayer2();
-                ShowTexturePlayer2();
+            if(PhotonNetwork.player.ID == 1) {
+                SendPlayer1Props();
+                localDebugText.text = player2remote.ToString();
+                GetPlayer2Props();
+            }
+            else if(PhotonNetwork.player.ID == 2) {
+                SendPlayer2Props();
+                localDebugText.text = player1remote.ToString();
+                GetPlayer1Props();
             }
 
         }
@@ -55,13 +71,16 @@ public class CameraSeeTroughScript : PunBehaviour {
         }
 
         float ratio = (float)backCam.width / (float)backCam.height;
-        fit.aspectRatio = ratio;
+        localFitter.aspectRatio = ratio;
+        remoteFitter.aspectRatio = ratio;
 
         float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
-        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+        localBackground.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+        remoteBackground.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
 
         int orient = -backCam.videoRotationAngle;
-        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+        localBackground.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+        remoteBackground.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
 
         if (!cameraOn && (OVRInput.Get(OVRInput.Button.Any) || Input.GetKeyDown("space")))
         {
@@ -69,7 +88,16 @@ public class CameraSeeTroughScript : PunBehaviour {
             cameraOn = true;
             localCanvas.renderMode = RenderMode.ScreenSpaceCamera;
             remoteCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
-            Debug.Log("Switched to localCanvas");
+            TakeSnapshot();
+            if (PhotonNetwork.player.ID == 1)
+            {
+                player1remote = false;
+            }
+            else if (PhotonNetwork.player.ID == 2)
+            {
+                player2remote = false;
+            }
+            // Debug.Log("Switched to localCanvas");
         }
         else if (cameraOn && (OVRInput.Get(OVRInput.Button.Any) || Input.GetKeyDown("space")))
         {
@@ -77,13 +105,23 @@ public class CameraSeeTroughScript : PunBehaviour {
             cameraOn = false;
             localCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
             remoteCanvas.renderMode = RenderMode.ScreenSpaceCamera;
-            Debug.Log("Switched to remoteCanvas");
+            if (PhotonNetwork.player.ID == 1)
+            {
+                player1remote = true;
+            }
+            else if (PhotonNetwork.player.ID == 2)
+            {
+                player2remote = true;
+            }
+            // Debug.Log("Switched to remoteCanvas");
         }
+
+        localDebugText.text = arrayLength.ToString();
 	}
 
     void InitCamera()
     {
-        defaultBackground = background.texture;
+        defaultBackground = localBackground.texture;
         WebCamDevice[] devices = WebCamTexture.devices;
 
         if (devices.Length == 0)
@@ -108,52 +146,111 @@ public class CameraSeeTroughScript : PunBehaviour {
         }
 
         backCam.Play();
-        background.texture = backCam;
+        localBackground.texture = backCam;
 
         camAvailable = true;
     }
 
-    private void SetTexturePlayer1()
+    private void SendPlayer1Props()
     {
         if (PhotonNetwork.connected)
         {
-            hashtable["player1Texture"] = backCam;
+            hashtable["player1Remote"] = player1remote;
+            if (player2remote) {
+                hashtable["player1Texture"] = EncodePNG();
+            }
 
             PhotonNetwork.room.SetCustomProperties(hashtable);
-            Debug.Log("Player 1 texture sent");
         }
 
     }
 
-    private void ShowTexturePlayer1()
+    private void GetPlayer2Props()
     {
         if (PhotonNetwork.connected)
         {
-            WebCamTexture player2Texture = (WebCamTexture)PhotonNetwork.room.CustomProperties["player2Texture"];
-            remotePlane.texture = player2Texture;
-            Debug.Log("Player 2 texture received");
+            player2remote = (bool)PhotonNetwork.room.CustomProperties["player2Remote"];
+
+            byte[] player2Texture;
+            player2Texture = null;
+
+            if(player1remote) {
+                player2Texture = (byte[])PhotonNetwork.room.CustomProperties["player2Texture"];
+                DecodePNG(player2Texture);
+            }
+
+            /*if (player2Texture == null)
+            {
+                localDebugText.text = "null";
+            }
+            else
+            {
+                localDebugText.text = "not null";
+            }*/
         }
     }
 
-    private void SetTexturePlayer2()
+    private void SendPlayer2Props()
     {
         if (PhotonNetwork.connected)
         {
-            hashtable["player2Texture"] = backCam;
+            hashtable["player2Remote"] = player2remote;
+            if (player1remote)
+            {
+                hashtable["player2Texture"] = EncodePNG();
+            }
 
             PhotonNetwork.room.SetCustomProperties(hashtable);
-            Debug.Log("Player 2 texture sent");
         }
-
     }
 
-    private void ShowTexturePlayer2()
+    private void GetPlayer1Props()
     {
         if (PhotonNetwork.connected)
         {
-            WebCamTexture player1Texture = (WebCamTexture)PhotonNetwork.room.CustomProperties["player1Texture"];
-            remotePlane.texture = player1Texture;
-            Debug.Log("Player 1 texture received");
+            player1remote = (bool)PhotonNetwork.room.CustomProperties["player1Remote"];
+
+            byte[] player1Texture;
+            player1Texture = null;
+
+            if (player2remote)
+            {
+                player1Texture = (byte[])PhotonNetwork.room.CustomProperties["player1Texture"];
+                DecodePNG(player1Texture);
+            }
+
+            /*if(player1Texture == null){
+                localDebugText.text = "null";
+            } else{
+                localDebugText.text = "not null";
+            }*/
         }
+    }
+
+    private byte[] EncodePNG(){
+        Texture2D texture2d = new Texture2D(backCam.width, backCam.height);
+        //texture2d.Resize(texture2d.width/16, texture2d.height/16, texture2d.format, false);
+        texture2d.Apply();
+        texture2d.SetPixels(backCam.GetPixels());
+
+        return texture2d.EncodeToJPG(20);
+    }
+
+    private void DecodePNG(byte[] receivedByte)
+    {
+        arrayLength = receivedByte.Length;
+        receivedTexture = null;
+        receivedTexture = new Texture2D(backCam.width, backCam.height);
+        receivedTexture.LoadImage(receivedByte);
+        remoteBackground.texture = receivedTexture;
+    }
+
+    private void TakeSnapshot(){
+        Texture2D snap = new Texture2D(backCam.width, backCam.height);
+        snap.Apply();
+        snap.SetPixels(backCam.GetPixels());
+
+        System.IO.File.WriteAllBytes(Application.persistentDataPath + "/snap" + _CaptureCounter.ToString() + ".png", snap.EncodeToPNG());
+        ++_CaptureCounter;
     }
 }
